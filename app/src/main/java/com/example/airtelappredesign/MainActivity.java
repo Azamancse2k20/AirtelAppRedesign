59ce4cbce5c6d9006a4af70cb994d0ba730fbff3
Ahaduzzaman <azamancse2k18@gmail.con>
2020-03-10 14:31:51 +0600

    third commit

--- app/src/main/java/com/example/airtelappredesign/MainActivity.java
+++ app/src/main/java/com/example/airtelappredesign/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.airtelappredesign;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/airtelappredesign/ProfilePage.java
+++ app/src/main/java/com/example/airtelappredesign/ProfilePage.java
@@ -1,14 +1,41 @@
 package com.example.airtelappredesign;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ProfilePage extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_page);
+
+        drawerLayout = findViewById(R.id.drawerLayoutId);
+        drawerLayout.setScrimColor(getResources().getColor(R.color.transparent));
+
+        navigationView = findViewById(R.id.navId);
+        toolbar = findViewById(R.id.toolbarId);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Home");
+        toolbar.setTitleTextColor(Color.WHITE);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open
+                , R.string.navigation_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.getDrawerArrowDrawable().setColor(Color.WHITE);
+        actionBarDrawerToggle.syncState();
+
     }
 }
